63f9c1dc0a41b1ce0c6f5f9e2d2c8bd1f1250ffa
coderiaser <coderaiser@cloudcmd.io>
2025-08-16 19:53:13 +0300

    feature: aleman: menu

---
+++ menu/menu.js
@@ -0,0 +1,49 @@
+createHydrate();
+createMap();
+
+await createLink();
+const {createMenu} = await import('../lib/main.js');
+
+createMenu();
+
+async function createLink() {
+    const style = document.createElement('style');
+    const content = await import('../style.css', {
+        with: {
+            type: 'css',
+        },
+    });
+    
+    for (const rule of content.default.cssRules) {
+        style.innerHTML += rule.cssText;
+    }
+    
+    document.head.appendChild(style);
+}
+
+function createHydrate() {
+    const section = document.createElement('section');
+    
+    section.dataset.name = 'hydrate';
+    section.innerHTML = `<ul data-name="menu" class="menu menu-hidden" style="left: 0; top: 20px;">
+        </ul>`;
+    document.body.append(section);
+}
+
+function createMap() {
+    const script = document.createElement('script');
+    
+    script.type = 'importmap';
+    script.innerHTML = `
+        {
+            "imports": {
+                "putout": "https://esm.sh/@putout/bundle",
+                "@putout/processor-html": "https://esm.sh/@putout/processor-html",
+                "html-escaper": "https://esm.sh/html-escaper",
+                "once": "https://esm.sh/once"
+            }
+        }
+    `;
+    document.body.append(script);
+}
+
