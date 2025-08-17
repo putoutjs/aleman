52689a164dea95c1477b62b5b8226938f851a098
coderiaser <coderaiser@cloudcmd.io>
2025-08-17 16:20:21 +0300

    fix: aleman: menu: get from state

--- menu/main.js
+++ menu/main.js
@@ -1,11 +1,11 @@
 import * as menuClick from '../lib//addons/show-menu-click.js';
 import * as menuItemClick from '../lib/addons/menu-item-click.js';
 import * as menuMouseOver from '../lib/addons/menu-mouse-over.js';
-import menu from '../lib//globals/menu.js';
+import menu from '../lib/globals/menu.js';
 import {hydrate} from '../lib/aleman/aleman.js';
 import {rules} from '../lib/rules/index.js';
 
-export function hydrateMenu() {
+export function hydrateMenu(element, options, menuData) {
     const addons = [
         menuClick,
         menuItemClick,
@@ -18,9 +18,13 @@ export function hydrateMenu() {
         'menu-toggler': {
             command: 'hide',
         },
+        'menu': {
+            data: menuData,
+        },
     };
     
     hydrate({
+        element,
         state,
         rules,
         addons,
