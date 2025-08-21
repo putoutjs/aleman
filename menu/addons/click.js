f93c7b2abc6af8d9c118c8fc6806e1c4e92e2403
coderiaser <coderaiser@cloudcmd.io>
2025-08-21 19:46:22 +0300

    fix: aleman: menu: beforeHide

--- menu/addons/click.js
+++ menu/addons/click.js
@@ -2,6 +2,8 @@ export const events = ['click'];
 
 export const listener = ({options, state}) => {
     options.beforeClick?.(state);
+    options.beforeHide?.(state);
+    
     return {
         command: 'hide',
         index: -1,
