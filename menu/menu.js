a02657dc08683b552e6e6dc1d96d2d2deb2073b5
coderiaser <coderaiser@cloudcmd.io>
2025-08-21 19:00:55 +0300

    fix: aleman: menu: import

--- menu/menu.js
+++ menu/menu.js
@@ -1,21 +1,26 @@
-import {importmap} from './importmap.js';
-import {hydrateMenu} from './hydrate-menu.js';
-
 const {stringify} = JSON;
 
 export const createMenu = async (element, options, menuData) => {
     const hydrateElement = createHydrate();
     
-    createMap();
+    await createMap();
     await createLink();
     
     createStateElement();
+    const {hydrateMenu} = await import('./hydrate-menu.js');
     
     hydrateMenu(hydrateElement, options, menuData);
 };
 
 async function createLink() {
+    const name = 'aleman-menu-style';
+    
+    if (findByName(name))
+        return;
+    
     const style = document.createElement('style');
+    
+    style.dataset.name = name;
     const content = await import('./menu.css', {
         with: {
             type: 'css',
@@ -30,9 +35,14 @@ async function createLink() {
 }
 
 function createHydrate() {
+    const name = 'aleman-hydrate-menu';
+    
+    if (findByName(name))
+        return;
+    
     const section = document.createElement('section');
     
-    section.dataset.name = 'aleman-hydrate-menu';
+    section.dataset.name = name;
     section.innerHTML = `<ul data-name="menu" class="menu menu-hidden"></ul>`;
     document.body.append(section);
     
@@ -40,17 +50,34 @@ function createHydrate() {
 }
 
 function createStateElement() {
+    const name = 'aleman-state-menu';
+    
+    if (findByName(name))
+        return;
+    
     const section = document.createElement('section');
     
+    section.dataset.name = name;
     section.classList.add('menu-hidden');
     section.dataset.name = 'aleman-state-menu';
     document.body.append(section);
 }
 
-function createMap() {
-    const script = document.createElement('script');
+async function createMap() {
+    const name = 'aleman-importmap';
     
+    if (findByName(name))
+        return;
+    
+    const script = document.createElement('script');
+    const {importmap} = await import('./importmap.js');
+    
+    script.dataset.name = name;
     script.type = 'importmap';
     script.innerHTML = stringify(importmap, null, 4);
     document.body.append(script);
 }
+function findByName(name) {
+    return document.querySelector(`[data-name=${name}]`);
+}
+
