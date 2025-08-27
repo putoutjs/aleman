63529f0210dffffcc5472e29f1989c32c0fa3caf
coderiaser <coderaiser@cloudcmd.io>
2025-08-27 18:06:42 +0300

    fix: aleman: menu: enter: filter

--- menu/addons/enter.js
+++ menu/addons/enter.js
@@ -2,6 +2,10 @@ const {values} = Object;
 
 export const key = 'Enter';
 
+export const filter = ({state}) => {
+    return state.command === 'show';
+};
+
 export const listener = ({options, state}) => {
     const {index} = state;
     const {menu} = options;
