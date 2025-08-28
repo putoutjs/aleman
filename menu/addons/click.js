bfb38aea254c3c7fab64e57d3aafd8756796143d
coderiaser <coderaiser@cloudcmd.io>
2025-08-28 18:59:02 +0300

    fix: aleman: menu: click

--- menu/addons/click.js
+++ menu/addons/click.js
@@ -1,5 +1,9 @@
 export const events = ['click'];
 
+export const filter = ({state}) => {
+    return state.command === 'show';
+};
+
 export const listener = ({options, state}) => {
     options.beforeClick?.(state);
     options.beforeHide?.(state);
