701253ec5b85115b33c7047a5221e1da6bf141dc
coderiaser <coderaiser@cloudcmd.io>
2025-09-04 22:35:57 +0300

    chore: aleman: v1.3.1

--- menu/rules/unselect-all/fixture/nested-fix.js
+++ menu/rules/unselect-all/fixture/nested-fix.js
@@ -6,10 +6,10 @@
     <li data-menu-path="New" data-name="menu-item" className='menu-item icon icon-edit menu-submenu-show'>
     <label data-menu-path="New">New</label>
     <ul data-name="menu" className="menu menu-hidden">
-        <li data-menu-path="New.File" data-name="menu-item" className='menu-item icon icon-view'>
+        <li data-menu-path="New.File" data-name="menu-item" className="menu-item icon icon-view">
         <label data-menu-path="New.File">File</label>
     </li>
-        <li data-menu-path="New.Directory" data-name="menu-item" className="menu-item icon icon-edit">
+        <li data-menu-path="New.Directory" data-name="menu-item" className='menu-item icon icon-edit'>
         <label data-menu-path="New.Directory">Directory</label>
     </li>
     </ul>
