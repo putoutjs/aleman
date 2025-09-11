7d84b9b3f76aeb190c7c6127b1b5d39a6c26317b
coderiaser <coderaiser@cloudcmd.io>
2025-09-11 22:03:41 +0300

    test: aleman: menu: sction -> section

--- menu/rules/menu/fixture/hide-menu-fix.js
+++ menu/rules/menu/fixture/hide-menu-fix.js
@@ -1,3 +1,3 @@
-<sction data-name="aleman-hydrate-hello">
+<section data-name="aleman-hydrate-hello">
     <ul data-name="menu" className="menu menu-hidden"><li></li><li></li></ul>;
-</sction>;
+</section>;
