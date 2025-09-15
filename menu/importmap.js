bf46b356bb12f5bdfe1a7b167fb8594282dc907c
coderiaser <coderaiser@cloudcmd.io>
2025-09-15 17:38:24 +0300

    fix: putout: v4.7.1

--- menu/importmap.js
+++ menu/importmap.js
@@ -1,6 +1,6 @@
 export const importmap = {
     imports: {
-        'putout': 'https://esm.sh/@putout/bundle@4.6.10',
+        'putout': 'https://esm.sh/@putout/bundle@4.7.1',
         '@putout/processor-html': 'https://esm.sh/@putout/processor-html',
         'fullstore': 'https://esm.sh/fullstore',
         'jessy': 'https://esm.sh/jessy',
