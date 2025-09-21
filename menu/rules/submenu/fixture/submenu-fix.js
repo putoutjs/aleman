bcbac169ab543f74c74be6136e9e3051dd652067
coderiaser <coderaiser@cloudcmd.io>
2025-09-21 14:27:14 +0300

    test: aleman: menu: update fixtures

--- menu/rules/submenu/fixture/submenu-fix.js
+++ menu/rules/submenu/fixture/submenu-fix.js
@@ -1,53 +1,53 @@
 // second selected
 <ul data-name="menu" class="menu menu-hidden">
     <li data-menu-path="Upload" data-name="menu-item" className="menu-item icon icon-view">
-    <label data-menu-path="Upload">Upload</label>
-</li>
+        <label data-menu-path="Upload">Upload</label>
+    </li>
     <li data-menu-path="New" data-name="menu-item" className="menu-item icon icon-edit menu-item-selected">
-    <label data-menu-path="New">New</label>
-    <ul data-name="menu" class="menu menu-hidden">
-        <li data-menu-path="New.File" data-name="menu-item" className="menu-item icon icon-view">
-        <label data-menu-path="New.File">File</label>
+        <label data-menu-path="New">New</label>
+        <ul data-name="menu" class="menu menu-hidden">
+            <li data-menu-path="New.File" data-name="menu-item" className="menu-item icon icon-view">
+                <label data-menu-path="New.File">File</label>
+            </li>
+            <li data-menu-path="New.Directory" data-name="menu-item" className="menu-item icon icon-edit menu-item-selected">
+                <label data-menu-path="New.Directory">Directory</label>
+            </li>
+        </ul>
     </li>
-        <li data-menu-path="New.Directory" data-name="menu-item" className="menu-item icon icon-edit menu-item-selected">
-        <label data-menu-path="New.Directory">Directory</label>
-    </li>
-    </ul>
-</li>
 </ul>;
 
 // first selected
 <ul data-name="menu" class="menu menu-hidden">
     <li data-menu-path="Upload" data-name="menu-item" className="menu-item icon icon-view menu-item-selected">
-    <label data-menu-path="Upload">Upload</label>
-</li>
+        <label data-menu-path="Upload">Upload</label>
+    </li>
     <li data-menu-path="New" data-name="menu-item" className="menu-item icon icon-edit">
-    <label data-menu-path="New">New</label>
-    <ul data-name="menu" class="menu menu-hidden">
-        <li data-menu-path="New.File" data-name="menu-item" className="menu-item icon icon-view">
-        <label data-menu-path="New.File">File</label>
+        <label data-menu-path="New">New</label>
+        <ul data-name="menu" class="menu menu-hidden">
+            <li data-menu-path="New.File" data-name="menu-item" className="menu-item icon icon-view">
+                <label data-menu-path="New.File">File</label>
+            </li>
+            <li data-menu-path="New.Directory" data-name="menu-item" className="menu-item icon icon-edit">
+                <label data-menu-path="New.Directory">Directory</label>
+            </li>
+        </ul>
     </li>
-        <li data-menu-path="New.Directory" data-name="menu-item" className="menu-item icon icon-edit">
-        <label data-menu-path="New.Directory">Directory</label>
-    </li>
-    </ul>
-</li>
 </ul>;
 
 // second selected, submenu first selected
 <ul data-name="menu" class="menu menu-hidden">
     <li data-menu-path="Upload" data-name="menu-item" className="menu-item icon icon-view">
-    <label data-menu-path="Upload">Upload</label>
-</li>
+        <label data-menu-path="Upload">Upload</label>
+    </li>
     <li data-menu-path="New" data-name="menu-item" className="menu-item icon icon-edit menu-item-selected">
-    <label data-menu-path="New">New</label>
-    <ul data-name="menu" class="menu menu-hidden">
-        <li data-menu-path="New.File" data-name="menu-item" className="menu-item icon icon-view">
-        <label data-menu-path="New.File">File</label>
+        <label data-menu-path="New">New</label>
+        <ul data-name="menu" class="menu menu-hidden">
+            <li data-menu-path="New.File" data-name="menu-item" className="menu-item icon icon-view">
+                <label data-menu-path="New.File">File</label>
+            </li>
+            <li data-menu-path="New.Directory" data-name="menu-item" className="menu-item icon icon-edit menu-item-selected">
+                <label data-menu-path="New.Directory">Directory</label>
+            </li>
+        </ul>
     </li>
-        <li data-menu-path="New.Directory" data-name="menu-item" className="menu-item icon icon-edit menu-item-selected">
-        <label data-menu-path="New.Directory">Directory</label>
-    </li>
-    </ul>
-</li>
 </ul>;
