9527fad2b074f2d3af1e210d2f38d64f66313bb1
coderaiser <coderaiser@users.noreply.github.com>
2025-11-07 16:16:29 +0000

    chore: aleman: lint with actions ☘️

--- nemo/addons/j/j.js
+++ nemo/addons/j/j.js
@@ -1,11 +1,10 @@
 import {updateState} from '../../state/state.js';
-import {getSubmenu} from '../submenu/index.js';
 
 export const filter = ({state}) => state.show;
 export const commands = ['j'];
 
 export function listener({count, state}) {
     return updateState('down', state, {
-        count
+        count,
     });
 }
