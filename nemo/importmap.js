dc0b6f375ed74aca27c9a49e4ea687381eef3f7d
coderiaser <coderaiser@cloudcmd.io>
2026-01-25 21:36:32 +0200

    feature: aleman: drop support of node < 22

--- nemo/importmap.js
+++ nemo/importmap.js
@@ -1,6 +1,6 @@
 export const importmap = {
     imports: {
-        'putout': 'https://esm.sh/@putout/bundle@4.7.6',
+        'putout': 'https://esm.sh/@putout/bundle@5.2.4',
         '@putout/processor-html': 'https://esm.sh/@putout/processor-html',
         'fullstore': 'https://esm.sh/fullstore',
         'jessy': 'https://esm.sh/jessy',
