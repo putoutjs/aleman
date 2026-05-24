5afafd134720f48af570890032a567a4c4fcad3c
coderiaser <coderaiser@cloudcmd.io>
2026-05-24 22:59:25 +0300

    feature: importmap: @putout/bundle@5.5.0

--- menu/importmap.js
+++ menu/importmap.js
@@ -1,6 +1,6 @@
 export const importmap = {
     imports: {
-        'putout': 'https://esm.sh/@putout/bundle@5.4.1',
+        'putout': 'https://esm.sh/@putout/bundle@5.5.0',
         '@putout/processor-html': 'https://esm.sh/@putout/processor-html',
         'fullstore': 'https://esm.sh/fullstore',
         'jessy': 'https://esm.sh/jessy',
