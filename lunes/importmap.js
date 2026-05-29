77b5f39490273f1a51a740f1a83a20267cf99738
coderiaser <coderaiser@cloudcmd.io>
2026-05-29 13:03:40 +0300

    chore: lunes: importmap: rm unused

--- lunes/importmap.js
+++ lunes/importmap.js
@@ -5,7 +5,5 @@ export const importmap = {
         'fullstore': 'https://esm.sh/fullstore',
         'jessy': 'https://esm.sh/jessy',
         '#aleman': '../aleman/aleman.js',
-        '#lunes/state/rules/apply-visibility': './lunes/state/rules/apply-visibility/index.js',
-        '#lunes/state/rules/move': './lunes/state/rules/move-cursor/index.js',
     },
 };
