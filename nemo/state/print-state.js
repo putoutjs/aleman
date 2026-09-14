765fcf789af2d701c08db9c084a6238626409149
coderaiser <coderaiser@cloudcmd.io>
2026-09-14 20:14:21 +0000

    feature: nemo: printState: print submenu children when open

--- nemo/state/print-state.js
+++ nemo/state/print-state.js
@@ -1,14 +1,16 @@
 export const printState = ({items}) => {
     const result = [];
-    
+
     for (const {selected, name, submenu} of items) {
-        const selectionMark = selected ? '+' : '-';
-        const submenuMark = submenu ? submenu.show ? '>' : '*' : '';
-        
-        const current = `${selectionMark}${name}${submenuMark}`;
-        
-        result.push(current);
+        const mark = selected ? '+' : '-';
+        const suffix = submenu ? (submenu.show ? '>' : '*') : '';
+
+        result.push(`${mark}${name}${suffix}`);
+
+        if (submenu?.show)
+            for (const {selected, name} of submenu.items)
+                result.push(`    ${selected ? '+' : '-'}${name}`);
     }
-    
+
     return result.join('\n');
 };
