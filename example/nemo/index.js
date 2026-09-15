7d2a2229d675aa6cece4c0cbef3c75cd86dbed0c
coderaiser <coderaiser@cloudcmd.io>
2026-09-15 07:03:36 +0000

    test: nemo: expose e2e hooks in nemo example

--- example/nemo/index.js
+++ example/nemo/index.js
@@ -1,19 +1,27 @@
 import {createMenu} from '../../nemo/nemo.js';
 
-globalThis.window.__fired = globalThis.window.__fired || [];
+globalThis.window.__fired = [];
 
-globalThis.window.__menu = {};
+let resolveMenu = null;
+const menuReady = new Promise((resolve) => {
+    resolveMenu = resolve;
+});
 
 const menuData = {
     hello: () => {
         globalThis.window.__fired.push('hello');
-        alert('x');
     },
     world: () => {
         globalThis.window.__fired.push('world');
-        alert('y');
     },
 };
 
-const menu = await createMenu(document.body, {}, menuData);
-Object.assign(globalThis.window.__menu, menu);
+globalThis.window.__menu = {
+    show: async (x, y) => (await menuReady).show(x, y),
+    hide: async () => (await menuReady).hide(),
+    ready: menuReady,
+};
+
+const menuInstance = await createMenu(document.body, {}, menuData);
+
+resolveMenu(menuInstance);
