d95a3c3e0195fd0b98e624bafa00b29a49a6f410
coderaiser <coderaiser@cloudcmd.io>
2026-09-16 22:53:36 +0000

    test: nemo: enable all specs in test runner

--- .madrun.js
+++ .madrun.js
@@ -2,24 +2,7 @@ import {run} from 'madrun';
 
 export default {
     'test': () => `tape '**/test/*.js' \
-   '**/{menu,aleman,lunes}/**/*.spec.js' \
-   'nemo/addons/left.spec.js' \
-   'nemo/addons/right.spec.js' \
-   'nemo/addons/set-position/calculate.spec.js' \
-   'nemo/rules/build-menu/index.spec.js' \
-   'nemo/state/state.spec.js' \
-   'nemo/addons/gg/gg.spec.js' \
-   'nemo/addons/shift-g/shift-g.spec.js' \
-   'nemo/addons/j/j.spec.js' \
-   'nemo/addons/k/k.spec.js' \
-   'nemo/addons/escape/escape.spec.js' \
-   'nemo/addons/click/click.spec.js' \
-   'nemo/addons/enter/enter.spec.js' \
-   'nemo/addons/item-click/item-click.spec.js' \
-   'nemo/addons/item-click/state.spec.js' \
-   'nemo/addons/mouse-leave/index.spec.js' \
-   'nemo/addons/down/down.spec.js' \
-   'nemo/state/parse-state.spec.js'`,
+   '**/{menu,aleman,lunes,nemo}/**/*.spec.js'`,
     'watch:test': async () => `nodemon -w lib -w test -x "${await run('test')}"`,
     'lint': () => `putout .`,
     'fresh:lint': () => run('lint', '--fresh'),
